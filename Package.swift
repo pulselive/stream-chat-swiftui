650cab03e2654b366d47f6acea17a57ecf66af20
Petr Pavlik <petrpavlik@Petrs-MacBook-Pro.local>
2022-09-21 15:04:31 +0200

    Update Nuke to 11

--- Package.swift
+++ Package.swift
@@ -17,14 +17,13 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.21.0"),        
-        .package(url: "https://github.com/kean/Nuke.git", from: "10.0.0"),
-        .package(url: "https://github.com/kean/NukeUI.git", from: "0.7.0")
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.21.0"),              
+        .package(url: "https://github.com/kean/Nuke.git", from: "11.1.0")
     ],
     targets: [
         .target(
             name: "StreamChatSwiftUI",
-            dependencies: [.product(name: "StreamChat", package: "stream-chat-swift"), "Nuke", "NukeUI"],
+            dependencies: [.product(name: "StreamChat", package: "stream-chat-swift"), "Nuke", .product(name: "NukeUI", package: "Nuke")],
             exclude: ["README.md", "Info.plist", "Generated/L10n_template.stencil"],
             resources: [.process("Resources")]
         )
